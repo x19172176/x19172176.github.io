4a1a3ee2c4bb91a81a55c521f1ac7f98dce89514
x19172176 <58444297+x19172176@users.noreply.github.com>
2020-08-12 17:14:32 +0100

    Update application.js

--- application.js
+++ application.js
@@ -64,7 +64,7 @@ getTasks();
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
+  var uluru = {lat: 53.3498, lng: - 6.2603};
   // The map, centered at Uluru
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 4, center: uluru});

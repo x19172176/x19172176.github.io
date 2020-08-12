c415e23a334e8533cf675588d1b88024d038aa17
x19172176 <58444297+x19172176@users.noreply.github.com>
2020-08-12 19:17:51 +0100

    Update application.js

--- application.js
+++ application.js
@@ -72,3 +72,8 @@ function initMap() {
   var marker = new google.maps.Marker({position: uluru, map: map});
 }
 
+function signOut() {
+  gapi.auth2.getAuthInstance().signOut().then(function() {
+    console.log('user signed out')
+  })
+}

26f19676afbef660420b49539c09382cdb182953
x19172176 <58444297+x19172176@users.noreply.github.com>
2020-08-12 15:12:17 +0100

    Update application.js

--- application.js
+++ application.js
@@ -3,6 +3,15 @@ var elementAdditionPosition = "beforeend";
 let auth0 = null;
 const fetchAuthConfig = () => fetch("/auth_config.json");
 
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+
+  auth0 = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId
+  });
+};
 
 function getTasks() {
   var xhttp = new XMLHttpRequest();

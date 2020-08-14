94bdac9ceba748b6883092479ba0ce8ec6b22812
x19172176 <58444297+x19172176@users.noreply.github.com>
2020-08-15 00:38:07 +0100

    Update application.js

--- application.js
+++ application.js
@@ -72,22 +72,4 @@ function initMap() {
   var marker = new google.maps.Marker({position: uluru, map: map});
 }
 
-var correctCaptcha = function(response) {
-        alert(response);
-    };
 
-document.getElementById("my_captcha_form").addEventListener("submit",function(evt)
-  {
-  
-  var response = grecaptcha.getResponse();
-  if(response.length == 0) 
-  { 
-    //reCaptcha not verified
-    alert("please verify you are humann!"); 
-    evt.preventDefault();
-    return false;
-  }
-  //captcha verified
-  //do the rest of your validations here
-  
-});
